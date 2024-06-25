f0e80d7c33e2f3a3ca88c213742b4afba9acd1fa
hshhh <109572141+hushh-h@users.noreply.github.com>
2024-06-25 20:47:40 +0800

    add utils/storage.js

---
+++ src/utils/storage.js
@@ -0,0 +1,27 @@
+const INFO_KEY = 'hm_shopping_info'
+const HISTORY_KEY = 'hm_history_list'
+
+export const getInfo = () => {
+  const defaultObj = { token: '', userId: '' }
+  const result = localStorage.getItem(INFO_KEY)
+  return result ? JSON.parse(result) : defaultObj
+}
+export const setInfo = (obj) => {
+  localStorage.setItem(INFO_KEY, JSON.stringify(obj))
+}
+export const removeInfo = () => {
+  localStorage.removeItem(INFO_KEY)
+}
+
+export const getHistoryList = () => {
+  const result = localStorage.getItem(HISTORY_KEY)
+  return result ? JSON.parse(result) : []
+}
+
+export const setHistoryList = (arr) => {
+  localStorage.setItem(HISTORY_KEY, JSON.stringify(arr))
+}
+
+export const removeHistoryList = () => {
+  localStorage.removeItem(HISTORY_KEY)
+}
